a79b6c54beb5a58cdc3e2b2eeb65ac05cdd94227
m4tx <m4tx@m4tx.pl>
2014-07-10 12:03:39 +0200

    Move navbar menu items to the right

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,8 @@
     <nav class="collapse navbar-collapse in" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+          wp_nav_menu(array('theme_location' => 'primary_navigation',
+              'menu_class' => 'nav navbar-nav navbar-right'));
         endif;
       ?>
     </nav>
