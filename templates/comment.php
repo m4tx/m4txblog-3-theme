59ebc4898d4fc92065f3d771a37452e1fdce9b3a
m4tx <m4tx@m4tx.pl>
2014-07-26 12:07:40 +0200

    Fix comment text appearance with zoom

--- templates/comment.php
+++ templates/comment.php
@@ -5,17 +5,20 @@
     <?php if ($comment->user_id == get_the_author_meta('ID')): ?>
       <span class="label label-danger"><?php _e('admin'); ?></span>
     <?php endif; ?>
-    <time data-toggle="tooltip" title="<?php echo get_comment_date('Y.m.d G:i:s') ?>" datetime="<?php echo comment_date('c'); ?>">
-      <a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(),  get_comment_time()); ?></a>
+    <time data-toggle="tooltip" title="<?php echo get_comment_date('Y.m.d G:i:s') ?>"
+          datetime="<?php echo comment_date('c'); ?>">
+      <a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s', 'roots'), get_comment_date(), get_comment_time()); ?></a>
     </time><?php edit_comment_link(__('(Edit)', 'roots'), '', ''); ?>
 
     <?php if ($comment->comment_approved == '0') : ?>
-      <p class="text-warning comment-awaiting-moderation"">
+      <p class="text-warning">
         <?php _e('Your comment is awaiting moderation.', 'roots'); ?>
       </p>
     <?php endif; ?>
 
-    <?php comment_text(); ?>
+    <div class="comment-text">
+      <?php comment_text(); ?>
+    </div>
     <?php echo str_replace('<a class=\'comment-reply-link', '<a class=\'btn btn-default btn-xs comment-reply-link',
         get_comment_reply_link(array_merge($args, array('depth' => $depth, 'max_depth' => $args['max_depth'])))); ?>
   </div>
